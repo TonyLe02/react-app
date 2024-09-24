a626d0e192b200e7fd3bdb10736fc94fa3833acd
Tony Nguyen Le <111498490+TonyLe02@users.noreply.github.com>
2024-09-24 23:35:31 +0200

    Refactor timeline component to update styling and colors

--- app/components/ui/timeline.tsx
+++ app/components/ui/timeline.tsx
@@ -62,13 +62,13 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
               <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-black dark:bg-black flex items-center justify-center">
                 <div className="h-4 w-4 rounded-full bg-green-500 dark:bg-green-500 border border-neutral-700 dark:border-neutral-700 p-2" />
               </div>
-              <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-500 dark:text-neutral-500 ">
+              <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-200 dark:text-neutral-200 ">
                 {item.title}
               </h3>
             </div>
 
             <div className="relative pl-20 pr-4 md:pl-4 w-full">
-              <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500">
+              <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-200 dark:text-neutral-200">
                 {item.title}
               </h3>
               {item.content}{" "}
@@ -86,7 +86,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
               height: heightTransform,
               opacity: opacityTransform,
             }}
-            className="absolute inset-x-0 top-0  w-[2px] bg-gradient-to-t from-purple-500 via-blue-500 to-transparent from-[0%] via-[10%] rounded-full"
+            className="absolute inset-x-0 top-0  w-[2px] bg-gradient-to-t from-yellow-500 via-red-500 to-transparent from-[0%] via-[10%] rounded-full"
           />
         </div>
       </div>
