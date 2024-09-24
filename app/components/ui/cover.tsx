12004820a3fcadaaee4c809f9693ccc7714e8de8
Tony Nguyen Le <111498490+TonyLe02@users.noreply.github.com>
2024-09-24 23:27:54 +0200

    Refactor cover and circle icons for consistent styling and dark mode support

--- app/components/ui/cover.tsx
+++ app/components/ui/cover.tsx
@@ -39,7 +39,7 @@ export const Cover = ({
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
       ref={ref}
-      className="relative hover:bg-[#121212]  group/cover inline-block dark:bg-[#121212] bg-neutral-100 px-2 py-2  transition duration-200 rounded-sm"
+      className="relative hover:bg-[#121212]  group/cover inline-block dark:bg-[#121212] bg-[#121212] px-2 py-2  transition duration-200 rounded-sm"
     >
       <AnimatePresence>
         {hovered && (
@@ -221,7 +221,7 @@ export const CircleIcon = ({
   return (
     <div
       className={cn(
-        `pointer-events-none animate-pulse group-hover/cover:hidden group-hover/cover:opacity-100 group h-2 w-2 rounded-full bg-neutral-600 dark:bg-white opacity-20 group-hover/cover:bg-white`,
+        `pointer-events-none animate-pulse group-hover/cover:hidden group-hover/cover:opacity-100 group h-2 w-2 rounded-full bg-neutral-400 dark:bg-white opacity-20 group-hover/cover:bg-white`,
         className
       )}
     ></div>
