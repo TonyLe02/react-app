f5ddcb8cc38bac0f81878eca69d5323c628913be
Tony Nguyen Le <111498490+TonyLe02@users.noreply.github.com>
2024-10-06 19:35:56 +0200

    refactor: Internship component to update Swipload company description

--- app/components/Internship.tsx
+++ app/components/Internship.tsx
@@ -30,7 +30,7 @@ const Internship = () => {
           assignments. By connecting contractors and drivers, it reduces empty
           trips and enhances efficiency, helping businesses save costs. Their
           platform integrates CO2 calculations, enabling users to track and
-          reduce emissions, making transport more eco-friendly. Swipload's
+          reduce emissions, making transport more eco-friendly. Swipload&apos;s
           solutions not only streamline logistics operations but also support
           sustainability efforts in the industry. With a focus on greener and
           smarter transportation, they are shaping the future of logistics.
