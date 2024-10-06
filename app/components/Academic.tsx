cc0ce35f3e7266930ef2d4cd6f1c7de1099acc12
Tony Nguyen Le <111498490+TonyLe02@users.noreply.github.com>
2024-10-06 22:03:33 +0200

    refactor: Update Academic component styles

--- app/components/Academic.tsx
+++ app/components/Academic.tsx
@@ -8,7 +8,7 @@ const Academic = () => {
   return (
     <section id="academic" className="bg-nero text-white p-8">
       <div className="container mx-auto">
-        <h2 className="text-xl md:text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-purple-500 to-purple-500 mb-2 leading-snug">
+        <h2 className="text-xl md:text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-purple-500 to-purple-500 mb-2 !leading-snug">
           Student Mentor and Teaching Assistant at UiA
         </h2>
 
