9874cfe5174db6585131361f0f4903f0a21a043d
Tony Nguyen Le <111498490+TonyLe02@users.noreply.github.com>
2024-10-06 20:49:26 +0200

    mui: Projects component to update programming project name

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -9,7 +9,7 @@ const Projects = () => {
     <section id="projects" className="text-white p-8">
       <div className="container mx-auto">
         <h2 className="text-xl md:text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-purple-500 to-purple-500 mb-2 md:leading-snug">
-          Programming Project
+          Programming Project - Nøsted &
         </h2>
         <h2 className="text-lg md:text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-purple-500 to-purple-500 mb-2 md:leading-snug">
           (Aug 2023 - 2024)
